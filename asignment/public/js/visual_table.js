40b231e05e270c658ca585994d9dfb6584e1f999
sitaram <sitaram@thoughtworks.com>
2016-11-19 08:31:32 +0530

    indentation

--- asignment/public/js/visual_table.js
+++ asignment/public/js/visual_table.js
@@ -8,12 +8,12 @@ var createTable = function() {
     var addHeader = function(title,scale){
         var type = 'th';
         var row  = table.append('tr');
-        var td = row.selectAll(type).data(title.concat(numbers))
+        var td = row.selectAll(type).data(title.concat(numbers));
         td.enter()
             .append(type)
             .text(function(d,i){ return i ? scale(d) : d ;})
             .classed('header',true);
-    }
+    };
 
     var addRow = function(title,scale,type){
         var header = d3.selectAll('.header');
@@ -21,11 +21,11 @@ var createTable = function() {
         var row  = table.append('tr');
         var data = header.data();
         data[0] = title[0];
-        var td = row.selectAll(type).data(data)
+        var td = row.selectAll(type).data(data);
         td.enter()
             .append(type)
             .text(function(d,i){ return i ? scale(d) : d ;})
-    }
+    };
 
     addHeader(title,d3.scaleIdentity());
     addRow(['n'],d3.scaleIdentity());
@@ -33,6 +33,6 @@ var createTable = function() {
     addRow(['log(n)'],d3.scaleLog());
     addRow(['log(n) Rounded'],d3.scaleLog().rangeRound([0,1]));
 
-}
+};
 
 window.load= createTable();
